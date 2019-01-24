c156e921b9754e85a278724146964eb97bb32e51
Francis Wardley <fwardley90@gmail.com>
2019-01-24 08:48:15 +0100

    texts and languages added

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { GlobalText } from '../../texts/global';
+
 @Component({
   selector: 'page-login',
   templateUrl: './login.html'
 })
 export class LoginPage {
 
+  public login = GlobalText.TEXTS;
+  public vendor = {username: '', password: ''}
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ngOnInit() {
+    // GlobalText.resetMenuMargin();
+}
+
+clickSubmit() {
+  console.log(this.vendor)
+}
 
 }
