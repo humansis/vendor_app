7946d90f3ca642da57ea8af3473801bdbd1ee9f1
Deploy Relief Applications <deployreliefapplications@Deploys-Mac-mini.local>
2019-03-08 17:22:47 +0100

    Automate sync

---
+++ declaration.d.ts
@@ -0,0 +1,23 @@
+// typescript < 2.8 does not include those
+
+interface HTMLElement { 
+    animate(keyframes: AnimationKeyFrame | AnimationKeyFrame[], options: number | AnimationOptions): Animation;
+}
+
+interface AnimationKeyFrame {
+    easing?: string | string[];
+    offset?: number | null | (number | null)[];
+    [index: string]: string | number | number[] | string[] | null | (number | null)[] | undefined;
+}
+
+interface AnimationOptions {
+    delay?: number;
+    direction?: "normal" | "reverse" | "alternate" | "alternate-reverse";
+    duration?: number;
+    easing?: string;
+    endDelay?: number;
+    fill?: "none" | "forwards" | "backwards" | "both" | "auto";
+    id?: string;
+    iterationStart?: number;
+    iterations?: number;
+}
